b4a345d3fdac87ceed3d5f53b79232677d2045bc
José Luís de Valle-Flôr <jose.luis.vf.tagus@gmail.com>
2019-08-21 17:23:49 +0100

    Add feature to use github api as main method of retrieving commits. This closes #10.

--- src/main/scala/com/joseluisvf/commitviewer/service/CommitHistoryService.scala
+++ src/main/scala/com/joseluisvf/commitviewer/service/CommitHistoryService.scala
@@ -11,51 +11,108 @@ import org.apache.logging.log4j.scala.Logging
 import scala.concurrent.ExecutionContext.Implicits.global
 import scala.concurrent._
 import scala.concurrent.duration._
+import scala.sys.process._
 import scala.util.{Failure, Success, Try}
 
-
+/**
+  * Responsible for setting up the HTTP endpoints for commit retrieval, which serve as the sole entry point to the application.
+  *
+  * The Github API is the first and main approach for commit retrieval. If that fails for any reason, the second approach is
+  * to use a fallback script [[com.joseluisvf.commitviewer.domain.commithistory.CommitRetrieverFallback]] to accomplish
+  * the same goal.
+  *
+  * There are two endpoints available. The second endpoint has two variants:
+  * Getting a list of commits for a public github repository
+  * - http://localhost:12345/commits/<GITHUB_REPOSITORY_URL>
+  * Getting a page of commits for a public github repository
+  * - http://localhost:12345/commits/<GITHUB_REPOSITORY_URL>/<PAGE_NUMBER>
+  * Getting a page of a user-defined amount of commits for a public github repository
+  * - http://localhost:12345/commits/<GITHUB_REPOSITORY_URL>/<PAGE_NUMBER>/<COMMITS_PER_PAGE>
+  *
+  */
 object CommitHistoryService extends Directives with Logging {
+  // This value sets expectations on how many elements we expect to find after splitting a valid URL on '/'s
+  private val EXPECTED_MINIMUM_URL_ELEMENT_COUNT_AFTER_SPLITTING = 5
   val route: Route = setupCommitHistoryRoute ~ setupPaginatedCommitHistoryRoute
 
-  def setupCommitHistoryRoute: Route = {
+  private def setupCommitHistoryRoute: Route = {
     path("commits" / Segment) {
       githubRepositoryUrl =>
         get {
-          getCommitHistoryOf(githubRepositoryUrl, CommitViewerCommon.DEFAULT_TIMEOUT_MILLISECONDS) match {
-            case Success(commits) =>
-              val commitsAsText = Commits.fromCommitsToText(commits)
-              complete(HttpEntity(ContentTypes.`text/html(UTF-8)`, commitsAsText))
+          try {
+            val commits: String = getCommitHistoryViaGithubOf(githubRepositoryUrl)
+            complete(HttpEntity(ContentTypes.`text/html(UTF-8)`, commits))
+          } catch {
+            case githubInvocationException: Exception =>
+              logger.error(s"Unable to invoke the Github API directly:\n ${githubInvocationException.getMessage}")
+              logger.info(s"Invoking the Github API directly failed. Resorting to using the fallback method.")
+              getCommitHistoryOf(githubRepositoryUrl, CommitViewerCommon.DEFAULT_TIMEOUT_MILLISECONDS) match {
+                case Success(commits) =>
+                  val commitsAsText = Commits.fromCommitsToText(commits)
+                  complete(HttpEntity(ContentTypes.`text/html(UTF-8)`, commitsAsText))
 
-            case Failure(e) =>
-              complete(StatusCodes.BadRequest, HttpEntity(ContentTypes.`text/html(UTF-8)`, s"FAIL:<h1>${e.getMessage}</h1>"))
+                case Failure(e) =>
+                  complete(StatusCodes.BadRequest, HttpEntity(ContentTypes.`text/html(UTF-8)`, s"FAIL:<h1>${e.getMessage}</h1>"))
+              }
           }
         }
     }
   }
 
-  def setupPaginatedCommitHistoryRoute: Route = {
+  private def setupPaginatedCommitHistoryRoute: Route = {
     path("commits" / Segment / IntNumber ~ Slash.? ~ IntNumber.?) {
       case (githubRepositoryUrl, pageNumber, maybeResultsPerPage) =>
         get {
-          var result = List.empty[Commit]
-          getCommitHistoryOf(githubRepositoryUrl, CommitViewerCommon.DEFAULT_TIMEOUT_MILLISECONDS) match {
-            case Success(commits) =>
-              maybeResultsPerPage match {
-                case Some(resultsPerPage) =>  result = PaginationAssistant(commits, resultsPerPage).getResultsFor(pageNumber)
-                case None => result = PaginationAssistant(commits).getResultsFor(pageNumber)
+          try {
+            val commits: String = getCommitHistoryViaGithubOf(githubRepositoryUrl, Some(pageNumber), maybeResultsPerPage)
+            complete(HttpEntity(ContentTypes.`text/html(UTF-8)`, commits))
+          } catch {
+            case githubInvocationException: Exception =>
+              logger.error(s"Unable to invoke the Github API directly:\n ${githubInvocationException.getMessage}")
+              logger.info(s"Invoking the Github API directly failed. Resorting to using the fallback method.")
+              getCommitHistoryOf(githubRepositoryUrl, CommitViewerCommon.DEFAULT_TIMEOUT_MILLISECONDS) match {
+                case Success(commits) =>
+                  val paginatedCommits: List[Commit] = maybeResultsPerPage match {
+                    case Some(resultsPerPage) => PaginationAssistant(commits, resultsPerPage).getCommitsFor(pageNumber)
+                    case None => PaginationAssistant(commits).getCommitsFor(pageNumber)
+                  }
+
+                  val commitsAsText = Commits.fromCommitsToText(paginatedCommits)
+                  complete(HttpEntity(ContentTypes.`text/html(UTF-8)`, commitsAsText))
+
+                case Failure(e) =>
+                  complete(StatusCodes.BadRequest, HttpEntity(ContentTypes.`text/html(UTF-8)`, s"FAIL:<h1>${e.getMessage}</h1>"))
               }
-
-              val commitsAsText = Commits.fromCommitsToText(result)
-              complete(HttpEntity(ContentTypes.`text/html(UTF-8)`, commitsAsText))
-
-            case Failure(e) =>
-              complete(StatusCodes.BadRequest, HttpEntity(ContentTypes.`text/html(UTF-8)`, s"FAIL:<h1>${e.getMessage}</h1>"))
           }
         }
     }
   }
 
-  private[this] def getCommitHistoryOf(githubRepositoryUrl: String, timeoutInMilliseconds: Long = CommitViewerCommon.DEFAULT_TIMEOUT_MILLISECONDS)
+  private[service] def getCommitHistoryViaGithubOf(githubRepositoryUrl: String, maybePageNumber: Option[Int] = None, maybeCommitsPerPage: Option[Int] = None)
+  : String = {
+    val split = githubRepositoryUrl.split("/")
+    require(split.size >= EXPECTED_MINIMUM_URL_ELEMENT_COUNT_AFTER_SPLITTING)
+    val owner = split(3)
+    val repository = split(4)
+
+    val paginationExtension = maybePageNumber match {
+      case Some(pageNumber) =>
+        maybeCommitsPerPage match {
+          case Some(commitsPerPage) => s"?page=$pageNumber&per_page=$commitsPerPage"
+          case None => s"?page=$pageNumber"
+        }
+
+      case None => ""
+    }
+
+    // scalastyle:off line.size.limit
+    val command = Seq("curl", "-H", "Content-Type: application/vnd.github.v3+json", "-H", "authorization: token 304916603ec4b262e15c2d1788db62aa8a969adf", s"https://api.github.com/repos/$owner/$repository/commits$paginationExtension")
+    // scalastyle:on line.size.limit
+    val result = command.!!
+    result
+  }
+
+  private[service] def getCommitHistoryOf(githubRepositoryUrl: String, timeoutInMilliseconds: Long = CommitViewerCommon.DEFAULT_TIMEOUT_MILLISECONDS)
   : Try[List[Commit]] = {
     try {
       Await.result(Future(getCommitHistoryOf(githubRepositoryUrl)), timeoutInMilliseconds.milliseconds)
@@ -67,7 +124,7 @@ object CommitHistoryService extends Directives with Logging {
     }
   }
 
-def getCommitHistoryOf(githubRepositoryUrl: String)
+private[this] def getCommitHistoryOf(githubRepositoryUrl: String)
   : Try[List[Commit]] = {
     if (!isUrlValid(githubRepositoryUrl)) {
       Failure(ErrorInvalidUrl(githubRepositoryUrl))
@@ -82,6 +139,6 @@ def getCommitHistoryOf(githubRepositoryUrl: String)
 
   private def isUrlValid(url: String): Boolean = {
     val validUrlPattern = "^https://[-a-zA-Z0-9+&@#/%?=~_|!:,.;]*[-a-zA-Z0-9+&@#/%=~_|]$"
-    url.matches(validUrlPattern)
+    url.matches(validUrlPattern) && url.split("/").length >= EXPECTED_MINIMUM_URL_ELEMENT_COUNT_AFTER_SPLITTING
   }
 }
--- src/test/scala/com/joseluisvf/commitviewer/service/PaginationAssistantTest.scala
+++ src/test/scala/com/joseluisvf/commitviewer/service/PaginationAssistantTest.scala
@@ -3,21 +3,19 @@ package com.joseluisvf.commitviewer.service
 import com.joseluisvf.commitviewer.domain.commit.Commit
 import org.scalatest.{Matchers, WordSpec}
 
-// scalastyle:off line.size.limit
 class PaginationAssistantTest extends WordSpec with Matchers {
   private val commitSeparator = Commit.placeholderSeparator
   private val validCommitAsText = s"abc${commitSeparator}123"
   private val aCommit = Commit(validCommitAsText)
   private val anotherCommit = Commit(validCommitAsText + "4")
 
-
   "A PaginationAssistant" when {
     "getPageContents is invoked" when {
       "a number of results per page exceeding the total amount is requested then all results should be returned" in {
         val commits = List(aCommit, aCommit, aCommit)
-        val paginationAssistant = PaginationAssistant(commits, 10)
+        val paginationAssistant = PaginationAssistant(commits)
 
-        val result = paginationAssistant.getResultsFor(1)
+        val result = paginationAssistant.getCommitsFor(1)
 
         result.size shouldEqual commits.length
       }
@@ -26,42 +24,47 @@ class PaginationAssistantTest extends WordSpec with Matchers {
         val commits = List(aCommit, aCommit, anotherCommit)
         val paginationAssistant = PaginationAssistant(commits, 1)
 
-        val result = paginationAssistant.getResultsFor(12)
+        val result = paginationAssistant.getCommitsFor(12)
 
         result.size shouldEqual 1
         result.head.getSubject shouldEqual "1234"
       }
+
       "there are not enough items to fill up a single page then they should all be returned" in {
         val commits = List(aCommit, aCommit, aCommit)
         val paginationAssistant = PaginationAssistant(commits, 100)
 
-        val result = paginationAssistant.getResultsFor(10)
+        val result = paginationAssistant.getCommitsFor(10)
 
         result.size shouldEqual commits.size
       }
 
       "a negative page is required then no results are returned" in {
         val commits = List(aCommit,aCommit,aCommit,aCommit,aCommit,aCommit,aCommit,aCommit,aCommit,aCommit,aCommit,aCommit,aCommit,aCommit, aCommit)
-        val paginationAssistant = PaginationAssistant(commits, 10)
+        val paginationAssistant = PaginationAssistant(commits)
 
-        val result = paginationAssistant.getResultsFor(-1)
+        val result = paginationAssistant.getCommitsFor(-1)
 
         result.size shouldEqual 0
       }
 
       "the last page is requested" when {
+        // scalastyle:off line.size.limit
         val aHundredCommits = List(aCommit, aCommit, aCommit, aCommit, aCommit, aCommit, aCommit, aCommit, aCommit, aCommit, aCommit, aCommit, aCommit, aCommit, aCommit, aCommit, aCommit, aCommit, aCommit, aCommit, aCommit, aCommit, aCommit, aCommit, aCommit, aCommit, aCommit, aCommit, aCommit, aCommit, aCommit, aCommit, aCommit, aCommit, aCommit, aCommit, aCommit, aCommit, aCommit, aCommit, aCommit, aCommit, aCommit, aCommit, aCommit, aCommit, aCommit, aCommit, aCommit, aCommit, aCommit, aCommit, aCommit, aCommit, aCommit, aCommit, aCommit, aCommit, aCommit, aCommit, aCommit, aCommit, aCommit, aCommit, aCommit, aCommit, aCommit, aCommit, aCommit, aCommit, aCommit, aCommit, aCommit, aCommit, aCommit, aCommit, aCommit, aCommit, aCommit, aCommit, aCommit, aCommit, aCommit, aCommit, aCommit, aCommit, aCommit, aCommit, aCommit, aCommit, aCommit, aCommit, aCommit, aCommit, aCommit, aCommit, aCommit, aCommit, aCommit, aCommit)
+        // scalastyle:on line.size.limit
 
         "the number of results is equal to the amount per page then all results should be returned" in {
-          val paginationAssistant = PaginationAssistant(aHundredCommits, 10)
-          val result = paginationAssistant.getResultsFor(9)
+          val paginationAssistant = PaginationAssistant(aHundredCommits)
+          val result = paginationAssistant.getCommitsFor(9)
 
           result.size shouldEqual 10
         }
 
-        "even if they are inferior to the results per page amount" in {
+        // scalastyle:off line.size.limit
+        "the number of results is equal to the amount per page then all results should be returned, even if they are inferior to the results per page amount" in {
+          // scalastyle:on line.size.limit
           val paginationAssistant = PaginationAssistant(aHundredCommits, 9)
-          val result = paginationAssistant.getResultsFor(12)
+          val result = paginationAssistant.getCommitsFor(12)
 
           result.size shouldEqual 1
         }
